8b448dadfe4323920d9122cd1227e35f1113570b
ChugnasIvan <ivan@ad-comp.net>
2023-01-17 23:43:17 -0500

    @ChugnasIvan - primer commit

---
+++ src/main/java/com/pluton/veterianaria/PlutonConectVeterianariasApplication.java
@@ -0,0 +1,13 @@
+package com.pluton.veterianaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlutonConectVeterianariasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlutonConectVeterianariasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pluton/veterianaria/PlutonConectVeterianariasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pluton.veterianaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlutonConectVeterianariasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
