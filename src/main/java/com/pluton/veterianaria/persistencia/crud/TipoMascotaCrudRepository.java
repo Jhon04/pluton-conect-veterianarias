e78628ab26cf5073ec667ed3558b53ad6a895cf1
rdasilva <roommaterds@canvia.com>
2023-01-24 00:32:57 -0500

    correccion de error crudRepository

--- src/main/java/com/pluton/veterianaria/persistencia/crud/TipoMascotaCrudRepository.java
+++ src/main/java/com/pluton/veterianaria/persistencia/crud/TipoMascotaCrudRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
-public interface TipoMascotaCrudRepository extends CrudRepository<TipoMascotaCrudRepository, Integer> {
+public interface TipoMascotaCrudRepository extends CrudRepository<TipoMascota, Integer> {
 
 
 }
